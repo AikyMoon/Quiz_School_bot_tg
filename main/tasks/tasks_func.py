9beb90816a6c86f2482005ab6d7ad4e8fa0ebcee
Aiky <tuzat369@gmail.com>
2022-09-07 11:16:53 +0300

    update check_answer func

--- main/tasks/tasks_func.py
+++ main/tasks/tasks_func.py
@@ -17,7 +17,7 @@ def task_bind(task_id: int, user_id: int):
 def check_answer(task_id: int, answer: str) -> bool:
     cur.execute(f"select answer from questions where task_id = {task_id}")
     correct = cur.fetchone()[0]
-    return correct in answer
+    return answer in correct
 
 
 # ------------------ получение количеста очков          ------------------
